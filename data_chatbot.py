e13867103048b5055bb2ffe59d4bc473d526a203
ppgg88 <paul.giroux87@gmail.com>
2021-04-26 16:52:32 +0200

    implementation chatbot

---
+++ data_chatbot.py
@@ -0,0 +1,74 @@
+import configparser
+import csv
+
+cfg = configparser.ConfigParser()
+cfg.read('./config/info.ini')
+
+data = [
+    [
+        ["quel est ton nom", "comment t'appelles-tu", "qui es-tu", "comment te prenomme tu", "quel est ton prénom"],
+        [("mon nom est " + cfg["robot"]["robot_name"]), ("je m'appelle "  + cfg["robot"]["robot_name"])]
+    ],
+    [
+        ["quel est ton âge", "quel âge as-tu", "depuis combien de temps es-tu né"],
+        [("j'ai " + cfg["robot"]["robot_age"] + "ans"), ("j'ai " + cfg["robot"]["robot_age"] + "ans, mais l'age ne compte pas vraiment pour un robot !")]
+    ],
+    [
+        ["kit à fabriquer", "qui est ton meilleur", "qui t'a fabriqué", "quelle est la personne qui t'a fabriqué", "qui est ton maker"],
+        [("c'est " + cfg["robot"]["maker_name"] + "qui m'a fabriquer, j'apartien à la grande famille de robot InMouv")]
+    ],
+    [
+        [("qui est " + cfg["robot"]["maker_name"]), ("connais-tu " + cfg["robot"]["maker_name"]), ("sais-tu qui est " + cfg["robot"]["maker_name"]),("voiture qui est " + cfg["robot"]["maker_name"]),("vois-tu qui est" + cfg["robot"]["maker_name"])],
+        ["bien sur il s'agit de la perssone qui ma construite", "oui c'est un peut comme un membre de ma famille sans lui je n'existerais pas"]
+    ],
+    [
+        ["as-tu des cheveux", "quelle est la couleur de tes cheveux", "de quelle couleur es-tu", "quelle est ta couleur", "comment t'habitues", "comment t'habilles tu"],
+        [("celas depent de l'humeur de " + cfg["robot"]["maker_name"]), ("en fonction de l'humeur de " + cfg["robot"]["maker_name"])]
+    ],
+    [
+        ["as-tu des enfants", "es-tu père", "es-tu papa", "et du perds"],
+        ["je suis un robot je n'ai pas d'enfants", "cette question est etrange, je suis un robot"]
+    ],
+    [
+        ["où es-tu né", "quel est ton lieu de naissance", "où tu es né"],
+        ["je suis née sur une imprimante 3D comme tout les autre membre de la famille de robot InMouv"]
+    ],
+    [
+        ["quelle est la capitale de la France", "quelle est la capitale française", "quelle est la capitale de la République française"],
+        ["c'est Paris", "la capitale de la france est paris", "paris est la capitale de la france"]
+    ],
+    [
+        ["Bonjour", ("Bonjour " + cfg["robot"]["robot_name"]), "Salut", ("Salut " + cfg["robot"]["robot_name"])],
+        ["Bonjour, que puit-je faire pour vous ?", "Bonjour, je peut vous aider ?", "Bonjour, je suis comptemps de vous voir"]
+    ],
+    [
+        ["Bonjour comment vas-tu", ("Bonjour " + cfg["robot"]["robot_name"] + " comment vas-tu"), "Salut comment vas-tu", ("Salut " + cfg["robot"]["robot_name"] + " comment vas-tu")],
+        ["bonjour, je vais bien mais tu sais je suis un robot ! et toi comment va-tu ?", "bonjour, tu sais je suis un robot je vais toujour bien ! et toi comment va-tu ?"]
+    ],
+    [
+        ["ça va", "tout va bien", "je vais bien", "tu vas bien"],
+        ["c'est une bonne nouvelle ! je peut faire quelque chose pour toi ?"]
+    ],
+    [
+        ["ça ne va pas", "je vais mal", "je ne me sent pas bien"],
+        ["ail ! si je peut faire quelque chose pour toi n'hesite pas !"]
+    ],
+    [
+        ["merci","merci beaucoup","merci à toi","merci bien",],
+        ["de rien !", "de rien, je peut faire autre chose ?", "de rien, je peut faire autre chose pour toi ?"]
+    ],
+    [
+        [cfg["robot"]["robot_name"]],
+        ["oui ? je peut faire quelque chose ?", "je suis là !", "c'est mon nom !", "c'est comme ça que les gents m'apelle"]
+    ],
+
+]
+
+data_mots_suplementaire = [
+    "merci",
+    "merci beaucoup",
+    "merci à toi",
+    "merci bien",
+    "s'il te plaît",
+    cfg["robot"]["robot_name"]
+]
--- speechrecognition_.py
+++ speechrecognition_.py
@@ -1,22 +1,52 @@
 import speech_recognition as sr
+from data_chatbot import *
+from random import randint
+import os
+from arduino import *
 
-rec_vocal = sr.Recognizer()
-mic = sr.Microphone()
-rec_vocal.pause_threshold = 0.5
-rec_vocal.non_speaking_duration = 0.4
+def say(text):
+    fichier = open("temp_lecture", "w")
+    fichier.write(text)
+    fichier.close()
+    deconnection_robot()
+    os.system("py speak.py")
+    connection_robot()
 
-init_mic = False
+def phrase_recognition(phrase):
+    reponse = True
+    for i in data :
+        for k in i[0]:
+            if phrase == k:
+                text = str(i[1][randint(0, len(i[1])-1)])
+                print("say -> " + text)
+                say(text)
+                reponse = False
+            else :
+                for j in data_mots_suplementaire:
+                    if phrase == (k + " " + j):
+                        text = str(i[1][randint(0, len(i[1])-1)])
+                        print("say -> " + text)
+                        say(text)
+                        reponse = False
+    if reponse :
+        say("je ne vous ai pas compris, il me reste encore beaucoup de chose à apprendre")
+
 
 def speak_recognition():
+    rec_vocal = sr.Recognizer()
+    mic = sr.Microphone()
+    rec_vocal.pause_threshold = 0.5
+    rec_vocal.non_speaking_duration = 0.4
     global init_mic
     with mic as src:
-        if init_mic == False:
-            rec_vocal.adjust_for_ambient_noise(src, duration=2)
-            print("ok")
-            init_mic = True
         audio = rec_vocal.listen(src)
         try :
             text = rec_vocal.recognize_google(audio, language="fr-FR")
-            print(text)
-        except:
+            print("reconaissance vocal ->" + str(text))
+            phrase_recognition(str(text))
+        except :
             pass
+
+if __name__ == "__main__":
+    while True :
+        speak_recognition()
